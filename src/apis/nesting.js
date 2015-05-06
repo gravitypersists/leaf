33e3037ab5475781aca0cf6d4995934cdd693c27
Michael Silveira <gravitypersists@gmail.com>
2015-05-06 16:21:29 -0700

    properly filter children according to what needs to be nested in nest calls

--- src/apis/nesting.js
+++ src/apis/nesting.js
@@ -8,9 +8,12 @@ class NestInterface {
   }
 
   nest(content, el) {
+    if (!this.element.children) return;
+
+    let children = [];
     let parsed = content.replace(/<<.+>>/, x => {
       let id =  x.replace('<<', '').replace('>>', '');
-      // let generatedId = id.split(':')[1];
+      children.push(id);
       return `<div 
                 class="leaf-layer"
                 data-leaf-node="${ id }"
@@ -18,17 +21,18 @@ class NestInterface {
               </div>`
     });
     $html(el, parsed);
-    let domList = el.querySelectorAll(':scope > .leaf-layer')
+
+    let groupedById = {};
+    this.element.children.forEach((child) => groupedById[child.id] = child);
+    let filteredChildren = children.map(childId => groupedById[childId]);
+
+    let domList = el.querySelectorAll(':scope > .leaf-layer');
     Array.prototype.forEach.call(domList, child => {
       let id = child.getAttribute('data-leaf-node');
-      // Is there an easier way to access static methods?
-      // This might be a mixin in the future anyways. 
-      new this.layer.constructor(this.element.children, this.element.layout, 0, child);
+      new this.layer.constructor(filteredChildren, this.element.layout, 0, child);
     });
   }
 
 }
-
-NestInterface
 
 module.exports = NestInterface;
---
+++ src/elements/image.js
@@ -0,0 +1,8 @@
+
+function Image(configuration, el, api) {
+
+  el.innerHTML = '<img src=' + configuration.source + '>';
+
+}
+
+module.exports = Image;
