d95f89e69b5f8f0d7e235d02c984b4899031021a
Michael Silveira <gravitypersists@gmail.com>
2015-05-21 23:02:09 -0700

    introduce paragraph spacing option for Document engine

--- src/layout-engines/document.js
+++ src/layout-engines/document.js
@@ -5,6 +5,11 @@ const $insert = require('../dom/insert');
 // Takes an array and lines them up into a list of block
 // level elements. Simple.
 
+let marginOptions = {
+  "singleSpacing": "0px",
+  "doubleSpacing": "1em"
+}
+
 // extends BaseLayoutEngine? No, I don't need to adhere.
 // Polymorphism is implied. Just don't fuck it up.
 class DocumentLayoutEngine {
@@ -16,9 +21,11 @@ class DocumentLayoutEngine {
   // must return an array of {id, el}
   render(parent) {
     parent.style.width = this.config.width || "auto";
+    let margins = marginOptions[this.config.paragraphSpacing || "singleSpacing"];
     return this.config['array'].map((id) => {
       let el = $insert(parent, `
-        <div style='display: block;'
+        <div style='display: block; 
+                    margin: ${ margins } 0px;'
              data-leaf-el='${ id }'
              class='leaf-element'>
         </div>
